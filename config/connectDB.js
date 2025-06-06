f331f2b4a360af318c4dfcfcd82a26dae46f6b36
Chaudhari Rohan <chaudharirohan2825@gmail.com>
2025-06-06 08:39:01 +0530

    Added cloud URL

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const connectDB = () => {
-    mongoose.connect(`${process.env.MONGODB_LOCAL}`)
+    mongoose.connect(`${process.env.MONGO_ALTAS_URI}`)
     .then(() => console.log("DB Connected"))
     .catch((err) => console.log("Error : ",err))
 }
